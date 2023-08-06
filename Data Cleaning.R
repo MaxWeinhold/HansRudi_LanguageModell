4a43a2e7f75c474b10bf1f4dccc3638dc8501d39
Maximilian Weinhold <78552945+MaxWeinhold@users.noreply.github.com>
2023-08-06 11:31:20 +0200

    Episode_8
    Advanced Camera Script

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,7 +1,7 @@
 #Ein Langauage Modell für Hans Rudi
 #Autor: Max Weinhold
 
-#Create Data Set----------------------------------------------------------------
+#Create Data Set for word classification----------------------------------------
 
 #Import Packages
 
@@ -10,6 +10,39 @@ rm(list=ls())
 
 #file location for all WhatsApp Chats
 setwd("C:/Users/MaxWe/Documents/WhatsAppChats")
+
+file = "CB_Chat.txt"
+x = readLines(file, encoding="UTF-8")
+for(j in 1:length(x)){
+  
+  
+  
+  
+  print(round(j/length(x)*100,2))
+  
+  y = strsplit(x[j], "\\ - | \\, |: ")
+  #strsplit(x[147], "\\ - | \\, |: ")
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #list of all chats
 files = c("JT_Chat.txt","CB_Chat.txt")
